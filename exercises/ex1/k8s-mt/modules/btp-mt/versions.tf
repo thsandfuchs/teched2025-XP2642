961e0f962ed8c34645afb4b327a4af0e66d9b87d
ptesny <rth@tiosouji.com>
2025-10-22 23:31:14 +0100

    ex1

--- exercises/ex1/k8s-mt/modules/btp-mt/versions.tf
+++ exercises/ex1/k8s-mt/modules/btp-mt/versions.tf
@@ -12,6 +12,12 @@ terraform {
       source = "salrashid123/http-full"
       version = "1.3.1"
     }        
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+    }  
+    kubectl = {
+      source  = "alekc/kubectl"
+    }
     qrcode = {
       source = "jackivanov/qrcode"
     }
--- exercises/ex1/k8s-mt/provider.tf
+++ exercises/ex1/k8s-mt/provider.tf
@@ -10,6 +10,12 @@ terraform {
     http-full = {
       source = "salrashid123/http-full"
     }     
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+    }  
     qrcode = {
       source = "jackivanov/qrcode"
       //version = "1.0.0"
@@ -30,4 +36,17 @@ provider "btp" {
   globalaccount  = var.BTP_GLOBAL_ACCOUNT
   cli_server_url = var.BTP_BACKEND_URL
   idp            = var.BTP_CUSTOM_IDP
+}
+
+provider "kubernetes" {}
+provider "kubectl" {}
+
+# https://developer.hashicorp.com/terraform/language/backend/kubernetes#configuration-variables
+# https://developer.hashicorp.com/terraform/language/backend/kubernetes#secret_suffix
+# the secret will be named in the format: tfstate-{workspace}-{secret_suffix}
+terraform {
+  backend "kubernetes" {
+    secret_suffix = "teched25"
+    namespace     = "quovadis-btp"
+  }
 }
